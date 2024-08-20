1ea762c98376677530754a8a6318e63367bb6791
DamikaSithuruwan <92681916+DamikaSithuruwan@users.noreply.github.com>
2024-08-20 11:55:57 +0530

    Add files via upload

---
+++ SQL QUERIES FOR BREAKDOWNS/TRIGGER FOR SHOW POINTS ON LOYALTY FORM/trgLoyaltyPointsUpdate.sql
@@ -0,0 +1,31 @@
+USE [easyway]
+GO
+
+/****** Object:  Trigger [dbo].[trgLoyaltyPointsUpdate]    Script Date: 2024-08-20 11:50:38 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER [dbo].[trgLoyaltyPointsUpdate]
+   ON [dbo].[tb_LoyaltyDet]
+   AFTER INSERT
+AS 
+BEGIN
+
+Update cl set cl.Points = lb.Points FROM tb_CustomerLoyalty cl
+Inner Join vw_LoyaltyBal lb on  cl.CustCode = lb.CustCode
+
+END
+GO
+
+ALTER TABLE [dbo].[tb_LoyaltyDet] ENABLE TRIGGER [trgLoyaltyPointsUpdate]
+GO
+
